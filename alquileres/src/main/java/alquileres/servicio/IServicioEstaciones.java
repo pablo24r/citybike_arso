fdd8bcca1b11ec29db0b2052a99605ec3353a9b8
pablo24r <94908598+pablo24r@users.noreply.github.com>
2024-04-09 20:26:51 +0200

    Delete alquileres directory

--- alquileres/src/main/java/alquileres/servicio/IServicioEstaciones.java
+++
@@ -1,8 +0,0 @@
-package alquileres.servicio;
-
-public interface IServicioEstaciones {
-
-	public boolean hayHuecoDisponible(String idEstacion);
-	
-	public boolean dejarBicicleta(String idUsuario, String idEstacion);
-}
