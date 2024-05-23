5d64552b764b581543940767c795e6cf5941aba5
pablo24r <94908598+pablo24r@users.noreply.github.com>
2024-05-24 00:12:41 +0200

    Delete alquileres-LopezGuevara directory

--- alquileres-LopezGuevara/src/main/java/alquileres/repositorio/RepositorioEstacionesMongo.java
+++
@@ -1,9 +0,0 @@
-package alquileres.repositorio;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import alquileres.modelo.Estacion;
-
-public interface RepositorioEstacionesMongo 
-	extends RepositorioEstaciones, MongoRepository<Estacion, String> {
-	
-}
